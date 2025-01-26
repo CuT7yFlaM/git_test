05d88f6fff48c687253a76fc7ce9d76f2d676c9c
LeoG <leo.galenski@posteo.net>
2025-01-27 00:01:03 +0100

    javascript first exercices

---
+++ javascript/var_op.js
@@ -0,0 +1,34 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+
+const pi = 3.14;
+
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1)) //auto math javascript
+
+console.log ((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+let x = "100";
+let y = "5";
+let z = x / y;
+console.log(z);
